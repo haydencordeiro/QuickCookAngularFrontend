2e0eb80beee46a8e8914509e40425efb0d1d3d4b
Hayden Cordeiro <cordeirohayden@gmail.com>
2024-04-27 16:36:41 -0400

    Added HttpClientModule to test dummy restapi

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.scss'
 })
-export class RecipesComponent {
+
+export class RecipesComponent implements OnInit {
+  recipes: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchRecipes();
+  }
+
+  fetchRecipes(): void {
+    this.http.get<any>('https://fakestoreapi.com/products?limit=5')
+      .subscribe(response => {
+        this.recipes = response;
+        console.log(response);
+        
+      });
+  }
+
+
 
 }
